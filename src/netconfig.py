b69b50a91567663496cd8c25a59a604dec5eb906
xhotsniperx <captain_akan_91@hotmail.com>
2017-12-14 13:23:38 -0800

    Started the to write the library. It now enables the forwarding.

--- src/netconfig.py
+++ src/netconfig.py
@@ -1,1 +1,11 @@
-print("Hello World")
+import subprocess
+
+print("Starting linux network configuration...")
+print("Enabling IP forwarding...")
+
+x = "sysctl -w net.ipv4.ip_forward=1"
+
+p = subprocess.Popen(x, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+for line in p.stdout.readlines():
+    print line,
+retval = p.wait()
