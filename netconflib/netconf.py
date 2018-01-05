6450cbd8108772da213ffaa7ca23db3439613aa5
Akan Yilmaz <captain_akan_91@hotmail.com>
2018-01-05 22:39:30 +0100

    changes for testing

--- netconflib/netconf.py
+++ netconflib/netconf.py
@@ -6,9 +6,9 @@ Use this library to simply configure the network topology of a linux cluster.
 
 import configparser
 import logging
+from treelib import Tree
 from .ssh import SSH
 from .commands import Commands
-from treelib import Node, Tree
 
 
 class NetConf:
@@ -32,6 +32,7 @@ class NetConf:
 
     cmds = None
     config = None
+    testing = False
 
     def __init__(self, configfile):
         self.logger = logging.getLogger('app.netconflib.NetConf')
@@ -44,65 +45,72 @@ class NetConf:
         self.num_nodes = len(self.nodes)
         self.username = self.config.get("authentication", "username")
         self.password = self.config.get("authentication", "password")
+        self.testing = self.config.get("settings", "testing")
 
-        self.logger.info("Testing connection to all specified nodes...")
-        try:
-            for node, addr in self.nodes:
-                self.logger.debug(
-                    "Creating new connection to {}, {} and adding it to list of connections...".format(node, addr))
-                self.connections.append(
-                    SSH(addr, self.username, self.password))
-        except Exception as ex:
-            self.logger.error(ex)
-            self.logger.error("connection test failed with {}.".format(node))
-        self.logger.info("Connection test finished.")
+        if not self.testing:
+            self.logger.info("Establishing connection to all specified nodes...")
+            try:
+                for node, addr in self.nodes:
+                    self.logger.debug(
+                        "Creating new connection to {}, {} and adding it to list of connections...".format(node, addr))
+                    self.connections.append(
+                        SSH(addr, self.username, self.password))
+            except Exception as ex:
+                self.logger.error(ex)
+                self.logger.error("connection failed with {}.".format(node))
+            self.logger.info("Connections established.")
 
     def enable_ip_forwarding(self):
         """Enables ip packet forwarding on every node on the cluster."""
         self.logger.info("Enabling IP packet forwarding...")
         for c in self.connections:
-            c.send_command(self.cmds.cmd_ipforward)
+            if not self.testing:
+                c.send_command(self.cmds.cmd_ipforward)
 
     def update_hosts_file(self):
         """Appends all the ip addresses and host names to etc/hosts file."""
-        for i, c in enumerate(self.connections):
+        for i in range(self.num_nodes):
             self.logger.debug(
                 "Node{}: Adding host entries to /etc/hosts file...".format(i + 1))
             for j in range(0, self.num_nodes):
                 if j == i:
                     self.logger.debug(
                         "127.0.1.1    {}".format(self.nodes[j][0]))
-                    c.send_command(
-                        "echo '127.0.1.1    {}' | sudo tee -a /etc/hosts".format(self.nodes[j][0]))
+                    if not self.testing:
+                        self.connections[i].send_command(
+                            "echo '127.0.1.1    {}' | sudo tee -a /etc/hosts".format(self.nodes[j][0]))
                 else:
                     self.logger.debug("{}   {}".format(
                         self.nodes[j][1], self.nodes[j][0]))
-                    c.send_command(
-                        "echo '{}   {}' | sudo tee -a /etc/hosts".format(self.nodes[j][1], self.nodes[j][0]))
+                    if not self.testing:
+                        self.connections[i].send_command(
+                            "echo '{}   {}' | sudo tee -a /etc/hosts".format(self.nodes[j][1], self.nodes[j][0]))
 
     def configure_ring_topology(self):
         """Configures the cluster's network topology as a ring."""
-        for i, c in enumerate(self.connections):
+        for i in range(self.num_nodes):
             self.logger.debug("Node{}:".format(i + 1))
             for j in range(0, self.num_nodes):
                 if j == i:
                     continue
-                self.logger.debug("route add -host {} gw {}".format(
+                self.logger.debug("sudo route add -host {} gw {}".format(
                     self.nodes[j][0], self.nodes[(i + 1) % (self.num_nodes)][0]))
-                c.send_command("sudo route add -host {} gw {}".format(
-                    self.nodes[j][0], self.nodes[(i + 1) % (self.num_nodes)][0]))
+                if not self.testing:
+                    self.connections[i].send_command("sudo route add -host {} gw {}".format(
+                        self.nodes[j][0], self.nodes[(i + 1) % (self.num_nodes)][0]))
 
     def remove_ring_topology(self):
         """Removes the ring netork topology from the cluster."""
-        for i, c in enumerate(self.connections):
+        for i in range(self.num_nodes):
             self.logger.debug("Node{}:".format(i + 1))
             for j in range(0, self.num_nodes):
                 if j == i:
                     continue
-                self.logger.debug("route del -host {} gw {}".format(
+                self.logger.debug("sudo route del -host {} gw {}".format(
                     self.nodes[j][0], self.nodes[(i + 1) % (self.num_nodes)][0]))
-                c.send_command("sudo route del -host {} gw {}".format(
-                    self.nodes[j][0], self.nodes[(i + 1) % (self.num_nodes)][0]))
+                if not self.testing:
+                    self.connections[i].send_command("sudo route del -host {} gw {}".format(
+                        self.nodes[j][0], self.nodes[(i + 1) % (self.num_nodes)][0]))
 
     def configure_star_topology(self, center, remove=False):
         """Configures the cluster's network topology as a star.
@@ -120,7 +128,7 @@ class NetConf:
         act = "add"
         if remove:
             act = "del"
-        for i, c in enumerate(self.connections):
+        for i in range(self.num_nodes):
             self.logger.debug("Node{}:".format(i + 1))
             if i == center:
                 self.logger.debug(
@@ -128,18 +136,20 @@ class NetConf:
                 for j in range(0, self.num_nodes):
                     if j == i:
                         continue
-                    self.logger.debug("route {} -host {} gw {}".format(
+                    self.logger.debug("sudo route {} -host {} gw {}".format(
                         act, self.nodes[j][0], self.nodes[j][0]))
-                    c.send_command("sudo route {} -host {} gw {}".format(
-                        act, self.nodes[j][0], self.nodes[j][0]))
+                    if not self.testing:
+                        self.connections[i].send_command("sudo route {} -host {} gw {}".format(
+                            act, self.nodes[j][0], self.nodes[j][0]))
             else:
                 for j in range(0, self.num_nodes):
                     if j == i:
                         continue
-                    self.logger.debug("route {} -host {} gw {}".format(
+                    self.logger.debug("sudo route {} -host {} gw {}".format(
                         act, self.nodes[j][0], self.nodes[center][0]))
-                    c.send_command("sudo route {} -host {} gw {}".format(
-                        act, self.nodes[j][0], self.nodes[center][0]))
+                    if not self.testing:
+                        self.connections[i].send_command("sudo route {} -host {} gw {}".format(
+                            act, self.nodes[j][0], self.nodes[center][0]))
 
     def configure_tree_topology(self, root, degree=2, remove=False):
         """Configures the cluster's network topology as a tree.
@@ -181,7 +191,7 @@ class NetConf:
         self.logger.info("The following tree will be configured:")
         tree.show()
 
-        for i, c in enumerate(self.connections):
+        for i in range(self.num_nodes):
             self.logger.debug("Node{}:".format(i + 1))
             subtree = tree.subtree(i)
             for j in range(self.num_nodes):
@@ -191,13 +201,15 @@ class NetConf:
                     children = tree.children(i)
                     for child in children:
                         if child.identifier == j or tree.is_ancestor(child.identifier, j):
-                            self.logger.debug("route {} -host {} gw {}".format(
+                            self.logger.debug("sudo route {} -host {} gw {}".format(
                                 act, self.nodes[j][0], self.nodes[child.identifier][0]))
-                            c.send_command("route {} -host {} gw {}".format(
-                                act, self.nodes[j][0], self.nodes[child.identifier][0]))
+                            if not self.testing:
+                                self.connections[i].send_command("sudo route {} -host {} gw {}".format(
+                                    act, self.nodes[j][0], self.nodes[child.identifier][0]))
                             break
                 elif tree.parent(i) != None:
-                    self.logger.debug("route {} -host {} gw {}".format(
+                    self.logger.debug("sudo route {} -host {} gw {}".format(
                         act, self.nodes[j][0], self.nodes[tree.parent(i).identifier][0]))
-                    c.send_command("route {} -host {} gw {}".format(
-                        act, self.nodes[j][0], self.nodes[tree.parent(i).identifier][0]))
+                    if not self.testing:
+                        self.connections[i].send_command("sudo route {} -host {} gw {}".format(
+                            act, self.nodes[j][0], self.nodes[tree.parent(i).identifier][0]))
