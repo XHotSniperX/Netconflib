41b63114ee7420f7918f35d9d0b8b13dc623de65
Akan Yilmaz <captain_akan_91@hotmail.com>
2018-01-17 21:59:13 +0100

    Updated Tests

--- tests/test_configurator.py
+++ tests/test_configurator.py
@@ -1,17 +1,39 @@
 import unittest
 import sys
+import configparser
 sys.path.append('../netconflib')
 from netconflib.netconf import NetConf
 
 
 class TestConfigurationMethods(unittest.TestCase):
+    """Tests the configuration methods of NetConf.
+    """
 
+    CONFIG_PATH = "tests/config.ini"
+    NUM = 64
+    config = None
     ncl = None
 
     def setUp(self):
-        self.ncl = NetConf("tests/config.ini")
+        self.config = configparser.ConfigParser()
+        config_file = open(self.CONFIG_PATH, "r")
+        self.config.read_file(config_file)
+        config_file.close()
+        self.config.remove_section("hosts")
+        self.config.add_section("hosts")
+        for i in range(self.NUM):
+            host = "node{}".format(i + 1)
+            address = "10.0.1.{}".format(i + 1)
+            self.config.set("hosts", host, address)
+        config_file = open(self.CONFIG_PATH, "w")
+        self.config.write(config_file)
+        config_file.close()
+        self.ncl = NetConf(self.CONFIG_PATH)
 
     def test_tree(self):
+        """Tests the tree configurator.
+        """
+
         try:
             self.ncl.configure_tree_topology(0, 2, False)
         except Exception:
