666d05fcdbeae7cc78f91bd5e4c64bc5db3d0692
Akan Yilmaz <captain_akan_91@hotmail.com>
2018-02-15 22:17:09 +0100

    Scapy fix

--- setup.py
+++ setup.py
@@ -127,7 +127,7 @@ setup(
     #
     # For an analysis of "install_requires" vs pip's requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['treelib', 'paramiko', 'scapy'],  # Optional
+    install_requires=['treelib', 'paramiko', 'scapy-python3'],  # Optional
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). Users will be able to install these using the "extras"
