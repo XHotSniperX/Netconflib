6d2013ff0d52f440708e2ada0ad1ac8cd472db99
Akan Yilmaz <captain_akan_91@hotmail.com>
2018-02-17 15:29:50 +0100

    codecov

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.3.1',  # Required
+    version='0.3.2',  # Required
 
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
@@ -127,7 +127,7 @@ setup(
     #
     # For an analysis of "install_requires" vs pip's requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['treelib', 'paramiko', 'scapy-python3'],  # Optional
+    install_requires=['treelib', 'paramiko', 'scapy>=2.4.0rc4'],  # Optional
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). Users will be able to install these using the "extras"
